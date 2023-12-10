bf0dc4f8719ad9bd902bbd7aee6b2357373f4b74
Dusan Uradnik <duradnik@redhat.com>
2023-12-11 00:52:27 +0100

    Add missing endpoint

---
+++ src/app/api/pokemon/route.tsx
@@ -0,0 +1,37 @@
+import { getServerSession } from 'next-auth';
+
+import { authOptions } from '@/server/auth';
+import { db } from '@/server/db';
+
+export const GET = async (req: Request) => {
+	const session = await getServerSession(authOptions);
+
+	if (!session) {
+		return new Response(JSON.stringify('Unauthorized.'), { status: 401 });
+	}
+
+	try {
+		const url = new URL(req.url, `http://${req.headers.get('host')}`);
+		const searchQuery = url.searchParams.get('query');
+
+		if (!searchQuery) {
+			return new Response(JSON.stringify('No search query provided.'), {
+				status: 400
+			});
+		}
+
+		const teams = await db.pokemon.findMany({
+			where: {
+				name: {
+					contains: searchQuery
+				}
+			}
+		});
+
+		return new Response(JSON.stringify(teams), { status: 200 });
+	} catch (e) {
+		return new Response(JSON.stringify('Something went wrong.'), {
+			status: 500
+		});
+	}
+};
