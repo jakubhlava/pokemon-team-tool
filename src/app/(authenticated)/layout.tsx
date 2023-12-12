883ba3323578b82605252b88135d9a787a1806b1
Dusan Uradnik <duradnik@redhat.com>
2023-12-12 10:54:46 +0100

    Modify navigation

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -1,16 +1,10 @@
 import { type PropsWithChildren } from 'react';
 
-import { LoginStatus } from '@/components/LoginStatus';
+import Navigation from '@/components/Navigation';
 
 const AuthenticatedLayout = ({ children }: PropsWithChildren) => (
 	<div className="flex flex-col gap-4">
-		<div className="flex h-16 items-center justify-between bg-white/60 p-4">
-			<span className="font-semibold text-emerald-900 lg:hidden">PTT</span>
-			<span className="hidden text-xl font-semibold text-emerald-900 lg:block">
-				Pokémon Team Tool
-			</span>
-			<LoginStatus />
-		</div>
+		<Navigation />
 		{children}
 	</div>
 );
